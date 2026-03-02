90f986ee7f2e4ad93e6a15123801f76cb71e2179
shadow578 <52449218+shadow578@users.noreply.github.com>
2026-03-02 18:17:44 +0100

    add ZCameraWaveFOVEffect

---
+++ src/Effects/Camera/ZCameraWaveFOVEffect.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Effects/Base/ZCameraEffectBase.h"
+
+class ZCameraWaveFOVEffect : public ZCameraEffectBase
+{
+  public:
+    void Start() override;
+
+    void OnDrawDebugUI() override;
+
+    void OnFrameUpdate(const SGameUpdateEvent& p_UpdateEvent, const float32 p_fEffectTimeRemaining) override;
+
+    std::string GetDisplayName() const override
+    {
+        return "Zoom Zoom Cam";
+    }
+
+  private:
+    float32 m_fOriginalFOV = -1.0f;
+    float32 m_fTimeSinceStart = 0.0f;
+
+    float32 m_fFrequency = 0.2f;
+    float32 m_fMinFOV = 20.0f;
+    float32 m_fMaxFOV = 120.0f;
+
+    float32 GetTargetFOV() const;
+};
