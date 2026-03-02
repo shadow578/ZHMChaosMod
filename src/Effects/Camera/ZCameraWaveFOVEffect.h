05ffa48e14d265d1019a3739d6e55757dbe6fe1c
shadow578 <52449218+shadow578@users.noreply.github.com>
2026-03-02 19:03:04 +0100

    fix ZCameraWaveFOVEffect GetDisplayName with wrong function signature

--- src/Effects/Camera/ZCameraWaveFOVEffect.h
+++ src/Effects/Camera/ZCameraWaveFOVEffect.h
@@ -10,7 +10,7 @@ class ZCameraWaveFOVEffect : public ZCameraEffectBase
 
     void OnFrameUpdate(const SGameUpdateEvent& p_UpdateEvent, const float32 p_fEffectTimeRemaining) override;
 
-    std::string GetDisplayName() const override
+    std::string GetDisplayName(const bool p_bVoting) const override
     {
         return "Zoom Zoom Cam";
     }
