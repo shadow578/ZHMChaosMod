a633017ab42254890ecefb5f09c2193bd1b29e60
shadow578 <52449218+shadow578@users.noreply.github.com>
2026-03-06 17:58:29 +0100

    move cripple box dbg effect to separate files, add weapon utils dbg effect

---
+++ src/Effects/Dbg/ZPlayerCrippleBoxDbgEffect.cpp
@@ -0,0 +1,98 @@
+#ifdef _DEBUG
+#include "ZPlayerCrippleBoxDbgEffect.h"
+
+#include <Logging.h>
+#include <imgui.h>
+
+#include "Registry.h"
+
+#define TAG "[ZPlayerCrippleBoxDbgEffect] "
+
+void ZPlayerCrippleBoxDbgEffect::OnDrawDebugUI()
+{
+    auto s_rCrippleBox = GetCrippleBox();
+    if (!s_rCrippleBox)
+    {
+        ImGui::TextUnformatted("CrippleBox not spawned, cannot access flags!");
+        return;
+    }
+
+    ImGui::Checkbox("Activate on Flag change", &m_bReapplyOnChange);
+
+    ImGui::SeparatorText("Flags");
+
+#define FLAG_CHKBOX(NAME)                                                                         \
+    if (ImGui::Checkbox(#NAME, &s_rCrippleBox.m_pInterfaceRef->NAME) && m_bReapplyOnChange)       \
+    {                                                                                             \
+        ActivateCrippleBox();                                                                     \
+        Logger::Debug(TAG #NAME " = {}", s_rCrippleBox.m_pInterfaceRef->NAME ? "true" : "false"); \
+    }
+
+#pragma region Flags
+    FLAG_CHKBOX(m_bInSequence);
+    FLAG_CHKBOX(m_bAllowBaseMovements);
+    FLAG_CHKBOX(m_bSequenceAllowCamera);
+    FLAG_CHKBOX(m_bLedges);
+    FLAG_CHKBOX(m_bCover);
+    FLAG_CHKBOX(m_bTakeClothes);
+    FLAG_CHKBOX(m_bLadder);
+    FLAG_CHKBOX(m_bPullVictimFromWindow);
+    FLAG_CHKBOX(m_bFiberWireKill);
+    FLAG_CHKBOX(m_bClimbWindow);
+    FLAG_CHKBOX(m_bThrowBodyOverRail);
+    FLAG_CHKBOX(m_bDumpBody);
+    FLAG_CHKBOX(m_bOperateCPDoor);
+    FLAG_CHKBOX(m_bHideInCloset);
+    FLAG_CHKBOX(m_bCloseCombat);
+    FLAG_CHKBOX(m_bGrabVictim);
+    FLAG_CHKBOX(m_bPushVictimThroughWindowAndRail);
+    FLAG_CHKBOX(m_bKickVictimOverLedge);
+    FLAG_CHKBOX(m_bPickupItem);
+    FLAG_CHKBOX(m_bDropItem);
+    FLAG_CHKBOX(m_bDragBody);
+    FLAG_CHKBOX(m_bThrowItem);
+    FLAG_CHKBOX(m_bPlaceItem);
+    FLAG_CHKBOX(m_bInteractions);
+    FLAG_CHKBOX(m_bUseDeathAnimation);
+    FLAG_CHKBOX(m_bVRTurn);
+    FLAG_CHKBOX(m_bVRThrow);
+    FLAG_CHKBOX(m_bRun);
+    FLAG_CHKBOX(m_bTurn);
+    FLAG_CHKBOX(m_bSneak);
+    FLAG_CHKBOX(m_bNoSnapSneak);
+    FLAG_CHKBOX(m_bStandUp);
+    FLAG_CHKBOX(m_bFastWalk);
+    FLAG_CHKBOX(m_bMovementAllowed);
+    FLAG_CHKBOX(m_bIdleAnimationsAllowed);
+    FLAG_CHKBOX(m_bItems);
+    FLAG_CHKBOX(m_bCanHolsterItems);
+    FLAG_CHKBOX(m_bCoverTakedown);
+    FLAG_CHKBOX(m_bCoverScale);
+    FLAG_CHKBOX(m_bCoverToCover);
+    FLAG_CHKBOX(m_bCloseCombatSnapNeck);
+    FLAG_CHKBOX(m_bCloseCombatChoke);
+    FLAG_CHKBOX(m_bCloseCombatPistolFinish);
+    FLAG_CHKBOX(m_bCloseCombatProps);
+    FLAG_CHKBOX(m_bCloseCombatStandard);
+    FLAG_CHKBOX(m_bCloseCombatFakeSwing);
+    FLAG_CHKBOX(m_bGameCameraAutoAlign);
+    FLAG_CHKBOX(m_bCameraSide);
+    FLAG_CHKBOX(m_bNotSelfieMode);
+    FLAG_CHKBOX(m_bInstinct);
+    FLAG_CHKBOX(m_bBlindFire);
+    FLAG_CHKBOX(m_bAim);
+    FLAG_CHKBOX(m_bHairTrigger);
+    FLAG_CHKBOX(m_bFire);
+    FLAG_CHKBOX(m_bLimitedAmmo);
+    FLAG_CHKBOX(m_bOpenLogbook);
+    FLAG_CHKBOX(m_bOpenPauseMenu);
+#pragma endregion
+
+#undef FLAG_CHKBOX
+
+    ImGui::SeparatorText("ZPlayerCrippleBoxEffectBase");
+    ZPlayerCrippleBoxEffectBase::OnDrawDebugUI();
+}
+
+REGISTER_CHAOS_EFFECT(ZPlayerCrippleBoxDbgEffect);
+#endif // _DEBUG
