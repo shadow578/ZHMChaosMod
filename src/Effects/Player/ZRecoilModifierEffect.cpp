a8532bba818cc75e4a778357bb6e4b9f4992c920
shadow578 <52449218+shadow578@users.noreply.github.com>
2026-06-06 15:01:54 +0200

    update ZRecoilModifierEffect to use parameterization adds HookingHelpers to allow detours in base classes

---
+++ src/Effects/Player/ZRecoilModifierEffect.cpp
@@ -0,0 +1,77 @@
+#include "ZRecoilModifierEffect.h"
+
+#include <imgui.h>
+
+#include <Glacier/ZHitman5.h>
+
+#include "Registry.h"
+#include "Helpers/Utils.h"
+
+void ZRecoilModifierEffect::OnModInitialized()
+{
+    if (Hooks::ZHM5WeaponRecoilController_RecoilWeapon)
+    {
+        Hooks::ZHM5WeaponRecoilController_RecoilWeapon->AddDetour(this, &ZRecoilModifierEffect::ZHM5WeaponRecoilController_RecoilWeapon);
+    }
+}
+
+void ZRecoilModifierEffect::OnModUnload()
+{
+    if (Hooks::ZHM5WeaponRecoilController_RecoilWeapon)
+    {
+        Hooks::ZHM5WeaponRecoilController_RecoilWeapon->RemoveDetour(&ZRecoilModifierEffect::ZHM5WeaponRecoilController_RecoilWeapon);
+    }
+}
+
+bool ZRecoilModifierEffect::Available() const
+{
+    return IChaosEffect::Available() && Hooks::ZHM5WeaponRecoilController_RecoilWeapon != nullptr;
+}
+
+bool ZRecoilModifierEffect::IsCompatibleWith(const IChaosEffect* p_pOtherEffect) const
+{
+    return IChaosEffect::IsCompatibleWith(p_pOtherEffect)
+           && !Utils::IsInstanceOf<ZRecoilModifierEffect>(p_pOtherEffect);
+}
+
+void ZRecoilModifierEffect::OnDrawDebugUI()
+{
+    ImGui::DragFloat2("Recoil Multiplier", &m_vRecoilMultiplier.x);
+}
+
+void ZRecoilModifierEffect::Start()
+{
+    m_bActive = true;
+}
+
+void ZRecoilModifierEffect::Stop()
+{
+    m_bActive = false;
+}
+
+DEFINE_BASE_CLASS_INSTANCES_HELPER(ZRecoilModifierEffect);
+
+DEFINE_BASE_CLASS_DETOUR(
+    ZRecoilModifierEffect,
+    void,
+    ZHM5WeaponRecoilController_RecoilWeapon,
+    MAKE_FORWARD_PARAMS(th, rWeapon),
+    ZHM5WeaponRecoilController* th,
+    const TEntityRef<ZHM5ItemWeapon>& rWeapon
+)
+{
+    if (!m_bActive)
+    {
+        return {HookAction::Continue()};
+    }
+
+    p_Hook->CallOriginal(th, rWeapon);
+
+    th->m_vRecoil.x *= m_vRecoilMultiplier.x;
+    th->m_vRecoil.y *= m_vRecoilMultiplier.y;
+
+    return {HookAction::Return()};
+}
+
+REGISTER_CHAOS_EFFECT_PARAM(zero, ZRecoilModifierEffect, "zero", "No Recoil", SVector2(0.f, 0.f));
+REGISTER_CHAOS_EFFECT_PARAM(extreme, ZRecoilModifierEffect, "extreme", "Extreme Recoil", SVector2(5.f, 5.f));
---
+++ src/Effects/Player/ZRecoilModifierEffect.h
@@ -0,0 +1,74 @@
+#pragma once
+#include "IChaosEffect.h"
+
+#include <Glacier/ZEntity.h>
+#include <Glacier/ZMath.h>
+
+#include "Helpers/HookingHelpers.h"
+
+class ZRecoilModifierEffect : public IChaosEffect
+{
+  public:
+    ZRecoilModifierEffect(
+        const std::string p_sNameSuffix,
+        const std::string s_sDisplayName,
+        const SVector2 p_vRecoilMultiplier,
+        const EDuration p_eDuration = EDuration::Full
+    )
+        : m_sNameSuffix(p_sNameSuffix),
+          m_sDisplayName(s_sDisplayName),
+          m_vRecoilMultiplier(p_vRecoilMultiplier),
+          m_eDuration(p_eDuration)
+    {
+        ADD_BASE_CLASS_DETOUR_INSTANCE();
+    }
+
+    ~ZRecoilModifierEffect()
+    {
+        REMOVE_BASE_CLASS_DETOUR_INSTANCE();
+    }
+
+    void OnModInitialized() override;
+    void OnModUnload() override;
+    bool Available() const override;
+    bool IsCompatibleWith(const IChaosEffect* p_pOtherEffect) const override;
+
+    void OnDrawDebugUI() override;
+
+    void Start() override;
+    void Stop() override;
+
+    std::string GetName() const override
+    {
+        return IChaosEffect::GetName() + "_" + m_sNameSuffix;
+    }
+
+    std::string GetDisplayName(const bool p_bVoting) const override
+    {
+        return m_sDisplayName;
+    }
+
+    EDuration GetDuration() const override
+    {
+        return m_eDuration;
+    }
+
+  private:
+    std::string m_sNameSuffix;
+    std::string m_sDisplayName;
+    EDuration m_eDuration;
+    SVector2 m_vRecoilMultiplier;
+
+  private:
+    bool m_bActive = false;
+
+    DECLARE_BASE_CLASS_INSTANCES_HELPER(ZRecoilModifierEffect);
+
+    DECLARE_BASE_CLASS_DETOUR(
+        ZRecoilModifierEffect,
+        void,
+        ZHM5WeaponRecoilController_RecoilWeapon,
+        ZHM5WeaponRecoilController* th,
+        const TEntityRef<ZHM5ItemWeapon>& p_rWeapon
+    );
+};
